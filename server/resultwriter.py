2cef9e17a70daa610b35c3ec27bda56f54b3a620
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-09-20 11:15:16 +0200

    Add Resultwriter class and first unittests

---
+++ server/resultwriter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+class ResultWriter:
+
+    def __init__(self, rabbit_host, result_exchange, task_exchange,
+                 icingacmd_path="/var/run/icinga2/cmd/icinga2.cmd"):
+        self._config = {"rabbit_host": rabbit_host,
+                       "result_exchange": result_exchange,
+                       "task_exchange": task_exchange}
+        self._icingacmd_path = icingacmd_path
+
+    def _get_icingacmd_string(self, check_result):
+        if check_result["icingacmd_type"] == "PROCESS_SERVICE_CHECK_RESULT":
+            icingacmd_string = "[{0}] PROCESS_SERVICE_CHECK_RESULT;{1};{2};{3};{4}" \
+                .format(check_result["time"],
+                        check_result["host"],
+                        check_result["name"],
+                        check_result["severity_code"],
+                        check_result["message"])
+        else:
+            icingacmd_string = "[{0}] PROCESS_HOST_CHECK_RESULT;{1};{2};{3}" \
+                .format(check_result["time"],
+                        check_result["host"],
+                        check_result["severity_code"],
+                        check_result["message"])
+        return icingacmd_string
+
+    def _get_icingacmd_commandline_string(self, icingacmd_string):
+        return "echo '"+icingacmd_string+"' >> "+self._icingacmd_path+";"
+
---
+++ test/test_resultwriter.py
@@ -0,0 +1,44 @@
+import unittest
+import yaml
+from server.resultwriter import ResultWriter
+import time
+
+class ResultwriterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        config_file = open("./test/appconfig.yml")
+        app_config = yaml.load(config_file)
+        self.config = {"rabbit_host": app_config.get("rabbit_host"),
+                       "result_exchange": app_config.get("result_exchange"),
+                       "task_exchange": app_config.get("task_exchange")}
+        self.resultwriter = ResultWriter(rabbit_host=self.config["rabbit_host"],
+                                    result_exchange=self.config["result_exchange"],
+                                    task_exchange=self.config["task_exchange"])
+
+    def test_set_rabbitmq_config(self):
+        self.assertEqual(self.resultwriter._config,
+                         self.config, "Setting rabbitMQ config failed" )
+
+    def test_icingacmd_string_formatter(self):
+        time_now = int(time.time())
+        check_result = {"icingacmd_type": "PROCESS_SERVICE_CHECK_RESULT",
+                        "time": str(time_now),
+                        "name": "administrator_ping_myself",
+                        "host": "administrator_itsclient",
+                        "severity_code": 1,
+                        "message": "OK - All fine"}
+        resultstring = "["+str(time_now)+"] PROCESS_SERVICE_CHECK_RESULT;administrator_itsclient;administrator_ping_myself;1;OK - All fine"
+        self.resultwriter._get_icingacmd_string(check_result)
+        self.assertEqual(self.resultwriter._get_icingacmd_string(check_result),
+                         resultstring,
+                         "Service check result string not properly formatted")
+        check_result["icingacmd_type"] = "PROCESS_HOST_CHECK_RESULT"
+        check_result["message"] = "Not Connected"
+        resultstring = "["+str(time_now)+"] PROCESS_HOST_CHECK_RESULT;administrator_itsclient;1;Not Connected"
+        self.assertEqual(self.resultwriter._get_icingacmd_string(check_result), resultstring,
+                         "Host check result string not properly formatted")
+
+    def test_icingacmd_command_formatter(self):
+        self.assertEqual(self.resultwriter._get_icingacmd_commandline_string("<ICINGACMD_STRING>"),
+                         "echo '<ICINGACMD_STRING>' >> "+self.resultwriter._icingacmd_path+";")
+
