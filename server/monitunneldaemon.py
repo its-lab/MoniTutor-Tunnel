c2ed4005f5b5324809bf426f9e833cc31fe06b44
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-09-21 10:35:00 +0200

    Refactor run function

--- server/monitunneldaemon.py
+++ server/monitunneldaemon.py
@@ -15,25 +15,25 @@ class MoniTunnelDaemon(Thread):
         while self.__running:
             while not self.__socket_open and self.__running:
                 try:
-                    self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                    self._socket.bind((self._config["address"], self._config["port"]))
-                    self._socket.listen(0)
+                    self._open_socket()
                     self.__socket_open = True
                 except socket.error:
                     self.__socket_open = False
-                    continue
-
             try:
-                self._socket.settimeout(1)
-                new_connection = self._socket.accept()
-                client = new_connection[0]
+                client, client_address = self._socket.accept()
+                print client_address
                 client.sendall("Hello student")
                 return True
             except socket.error as err:
                 if err != "timed out":
                     self.__socket_open = False
 
+    def _open_socket(self):
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.bind((self._config["address"], self._config["port"]))
+        self._socket.listen(0)
+        self._socket.settimeout(1)
 
     def stop(self):
         self.__running = False
