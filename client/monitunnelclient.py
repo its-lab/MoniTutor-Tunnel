29ef9701c7c77ed6513e5f47a3ac2930515e2f61
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-10-04 14:03:29 +0200

    Add authentication

--- client/monitunnelclient.py
+++ client/monitunnelclient.py
@@ -2,9 +2,9 @@ import subprocess
 import logging
 from threading import Thread
 from threading import Semaphore
-from threading import Lock
+from threading import Event
 import socket
-from queue import Queue
+from Queue import Queue
 import json
 import hashlib
 import hmac
@@ -25,35 +25,42 @@ class MonitunnelClient(Thread):
         self._socket_send_thread = Thread(target=self._socket_send, name="send")
         self._message_inbox = Queue()
         self._message_outbox = Queue()
+        self._socket_opened = Event()
+        self._socket_closed = Event()
+        self._socket_closed.clear()
+        self._socket_opened.clear()
+        self._message_inbox_lock = Semaphore(0)
+        self._message_outbox_lock = Semaphore(0)
+
 
     def run(self):
         self.__running = True
+        self._socket_receive_thread.start()
+        self._socket_send_thread.start()
         while self.__running:
             counter = 1
-            while not self.__connected:
+            while self.__running and not self.__connected:
                 self.__connected = self._connect_to_server()
                 time.sleep(2)
                 logging.info("Wasn't able to connect to server. Try "+str(counter)+"...")
                 counter+=1
-            self._message_inbox_lock = Semaphore(0)
-            self._message_outbox_lock = Semaphore(0)
-            self._socket_receive_thread = Thread(target=self._socket_receive, name="receive")
-            self._socket_send_thread = Thread(target=self._socket_send, name="send")
-            self._socket_lock = Lock()
-            self._socket_receive_thread.start()
-            self._socket_send_thread.start()
-            self._socket_lock.acquire()
-            self.__connected = False
-            self._message_inbox_lock.release()
-            self._message_outbox_lock.release()
-            self._socket_send_thread.join()
-            self._socket_receive_thread.join()
-            del self._socket_send_thread
-            del self._socket_receive_thread
+            if self.__running:
+                self._socket_opened.set()
+                self._authenticate()
+                self._socket_closed.wait()
+                self.__connected = False
 
+    def _authenticate(self):
+        message = {"method": "auth", "body": self._hostname}
+        self.send_message(message)
+
+    def send_message(self, message):
+        self._message_outbox.put(message)
+        self._message_outbox_lock.release()
 
     def _socket_receive(self):
-        while self.__connected:
+        self._socket_opened.wait()
+        while self.__running:
             try:
                 message = self._socket.recv(1024)
                 if message == "":
@@ -63,36 +70,46 @@ class MonitunnelClient(Thread):
                     self._message_inbox_lock.release()
             except socket.error as err:
                 if err.message == "timed out":
-                    continue
-                logging.exception("socket error while receive")
-                self.__connected = False
-                self._socket_lock.release()
+                    pass
+                else:
+                    logging.exception("socket error while receive")
+                    self._socket_opened.clear()
+                    self._socket_closed.set()
+            if self.__running:
+                self._socket_opened.wait()
 
     def _socket_send(self):
+        self._socket_opened.wait()
         self._message_outbox_lock.acquire()
-        while self.__connected:
-            message = self._message_outbox.get()
-            message = self._add_auth_header(message)
-            message = "\x02"+json.dumps(message)+"\x03"
+        while self.__running:
+            try:
+                message = self._message_outbox.get()
+                message = self._add_auth_header(message)
+                message = "\x02"+json.dumps(message)+"\x03"
+            except:
+                logging.exception("get and format message failed")
             try:
                 self._socket.send(message)
             except socket.error:
                 logging.exception("Socket error while send")
-                self.__connected = False
-                self._socket_lock.release()
+                self._socket_opened.clear()
+                self._socket_closed.set()
+            if self.__running:
+                self._socket_opened.wait()
+                self._message_outbox_lock.acquire()
 
     def _add_auth_header(self, message):
         serialized_message = json.dumps(message)
         hmac = self._get_hmac(serialized_message)
-        return {"HMAC": hmac, "ID": self._username, message: serialized_message}
+        return {"HMAC": hmac, "ID": self._username, "message": serialized_message}
 
     def _get_hmac(self, message):
         return hmac.new( self._hmac_secret, str(message), hashlib.sha256).hexdigest()
 
     def _connect_to_server(self):
         try:
-            self._socket = socket.create_connection(self._server_socket_config)
-            self._socket.settimeout(3)
+            self._socket = socket.create_connection(self._server_socket_config, 5)
+            self._socket.settimeout(2)
             return True
         except socket.error:
             logging.exception("Couldn't connect to server")
@@ -100,3 +117,11 @@ class MonitunnelClient(Thread):
 
     def stop(self):
         self.__running = False
+        self._socket_opened.set()
+        self._socket_closed.set()
+        self._message_inbox_lock.release()
+        self._message_outbox_lock.release()
+        self._socket_send_thread.join()
+        self._socket_receive_thread.join()
+
+
--- test/test_monitunnelclient.py
+++ test/test_monitunnelclient.py
@@ -2,6 +2,7 @@ from client.monitunnelclient import MonitunnelClient
 import socket
 import time
 import unittest
+import json
 
 
 class MonitunnelClientTestCase(unittest.TestCase):
@@ -35,15 +36,26 @@ class MonitunnelClientTestCase(unittest.TestCase):
         client_socket.shutdown(socket.SHUT_RDWR)
         del self._server_socket
         self.assertIs( tuple , type(self._start_server_and_return_clientsocket()), "Hard reset and reconnect failed")
+        del self._server_socket
 
     def test_message_authentication(self):
         self.client.start()
         client_socket, address = self._start_server_and_return_clientsocket()
-        client_socket.settimeout(1)
+        client_socket.settimeout(3)
         try:
             message = client_socket.recv(1024)
-        except:
-            del client_socket
+        except socket.error:
+            del self._server_socket
+            raise socket.error
+        else:
+            pass
+        self.assertIn("\x02", message, "Startstring control char missing")
+        self.assertIn("\x03", message, "Endofstring control char missing")
+        message = message.strip("\x02\x03")
+        message = json.loads(message)
+        self.assertIn( "HMAC", message.keys())
+        self.assertIn( "ID", message.keys())
+        self.assertIn( "message", message.keys())
         del self._server_socket
 
     def tearDown(self):
@@ -56,5 +68,5 @@ class MonitunnelClientTestCase(unittest.TestCase):
         self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self._server_socket.bind((self._server_address, int(self._server_port)))
         self._server_socket.listen(0)
-        self._server_socket.settimeout(3)
+        self._server_socket.settimeout(4)
         return self._server_socket.accept()
