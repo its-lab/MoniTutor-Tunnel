a6f9855b666c0460540c6bd81d79c96d709a093c
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2017-10-27 12:47:00 +0200

    Add monitunnel thread

--- start_server.py
+++ start_server.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import os
 import time
+from server.monitunneldaemon import MoniTunnelDaemon
 
 parser = argparse.ArgumentParser(description="MoniTunnel server")
 parser.add_argument("-a", "--address", default="0.0.0.0", help="Address to listen to", metavar="x.x.x.x")
@@ -12,9 +13,22 @@ parser.add_argument("-v", "--verbose", action="count", help="Increase verbosity"
 parser.add_argument("-l", "--logging", action="store_true", help="Write messages to syslog instead of stdout. Increase verbosity of logs with -v")
 parser.add_argument("-d", "--daemonize", action="store_true", help="Start as daemon")
 parser.add_argument("-s", "--ssl", action="store_true", help="Enable and enforce SSL")
+parser.add_argument("-u", "--db-user", help="Database user")
+parser.add_argument("-p", "--db-password",  help="Database password")
+parser.add_argument("-j", "--db-host", default="localhost", help="Address of the database host")
+parser.add_argument("-n", "--db-name", default="monitutor", help="Name of the database")
+parser.add_argument("-r", "--rabit-mq-host", default="localhost", help="Address of the rabbit-mq server")
 
 config = vars(parser.parse_args())
-print config
+
+daemon = MoniTunnelDaemon(port=config["port"],
+                          address=config["address"],
+                          rabbit_result_exchange="result_exchange",
+                          rabbit_task_exchange="task_exchange",
+                          db_host=congig["db-host"],
+                          db_password=config["db-password"],
+                          db_database=config["db-name"],
+                          db_username=config["db-user"])
 
 def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
     # Fork process to detach from exec console
@@ -64,6 +78,7 @@ def daemonize(stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
 
 def signal_handler(signum, frame):
     logging.warn("SIGNAL" + str(signum) + "received! Frame:" + str(frame))
+    daemon.stop()
     if config["daemonize"]:
         os.remove("/var/run/monitunnel.pid")
     sys.exit(0)
@@ -75,6 +90,8 @@ if "__main__" == __name__:
     signal.signal(signal.SIGALRM, signal_handler)
     signal.signal(signal.SIGHUP, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
+
+    daemon.start()
 
     run = True
 
