90829044aeea5c9375763adb5c3562bb9ca1a8bd
Mark Swillus <mark.swillus@alumni.fh-aachen.de>
2018-08-06 16:33:00 +0200

    Add second entry per object that stores latest result

--- server/couchDB_resultwriter.py
+++ server/couchDB_resultwriter.py
@@ -34,7 +34,33 @@ class CouchDbResultWriter(ResultWriter):
 
     def _write_result(self, result):
         logging.info("Write result: "+str(result))
-        self._database.create_document(result)
+        self._add_check_to_history(result)
+        self._update_check_object(result)
+
+    def _add_check_to_history(self, check_result):
+        hostname = check_result["hostname"]
+        if check_result["icingacmd_type"] == "PROCESS_SERVICE_CHECK_RESULT":
+            check_result["object_id"] = hostname+"_"+check_result["check"]["name"]
+        else:
+            check_result["object_id"] = hostname
+        self._database.create_document(check_result)
+        del check_result["object_id"]
+
+    def _update_check_object(self, check_result):
+        hostname = check_result["hostname"]
+        if check_result["icingacmd_type"] == "PROCESS_SERVICE_CHECK_RESULT":
+            check_result["_id"] = hostname+"_"+check_result["check"]["name"]
+        else:
+            check_result["_id"] = hostname
+        if check_result["_id"] in self._database:
+            existing_document = self._database[check_result["_id"]]
+            for key in check_result.viewkeys() & dict(existing_document).viewkeys():
+                logging.info("existing_doucument." + key + "=check_result." +
+                             key + " with value: " + str(check_result[key]))
+                existing_document[key] = check_result[key]
+            existing_document.save()
+        else:
+            self._database.create_document(check_result)
 
     def _init_couchDB(self):
         try:
@@ -42,10 +68,12 @@ class CouchDbResultWriter(ResultWriter):
         except KeyError:
             logging.info("Create database "+self._database_name)
             self._database = self._couch_client.create_database(self._database_name)
+            logging.info("Set revision limit of database "+self._database_name+"to 5")
+            self._database.set_revision_limit(5)
         design_doc = self._database.get_design_document("results")
-        check_results_map_function = """function(doc){
+        check_result_history_map_function = """function(doc){
   var time, output, hostname, severity, check_name, username;
-  if(doc.icingacmd_type == "PROCESS_SERVICE_CHECK_RESULT"){
+  if(doc.icingacmd_type == "PROCESS_SERVICE_CHECK_RESULT" && doc.object_id){
     hostname = doc.hostname.split("_")[1];
     username = doc.hostname.split("_")[0];
     check_name = doc.check.name;
@@ -55,9 +83,21 @@ class CouchDbResultWriter(ResultWriter):
     emit([username, check_name, time], {output: output, severity: severity});
   }
 }"""
-        host_status_map_function = """function(doc){
+        check_results_map_function = """function(doc){
+  var time, output, hostname, severity, check_name, username;
+  if(doc.icingacmd_type == "PROCESS_SERVICE_CHECK_RESULT" && !doc.object_id){
+    hostname = doc.hostname.split("_")[1];
+    username = doc.hostname.split("_")[0];
+    check_name = doc.check.name;
+    time = new Date(Number(doc.time)*1000);
+    output = doc.output;
+    severity = doc.severity_code;
+    emit([username, check_name], {output: output, severity: severity, time: time});
+  }
+}"""
+        host_status_history_map_function = """function(doc){
   var time, output, hostname, severity, username;
-  if(doc.icingacmd_type == "PROCESS_HOST_CHECK_RESULT"){
+  if(doc.icingacmd_type == "PROCESS_HOST_CHECK_RESULT" && doc.object_id){
     hostname = doc.hostname.split("_")[1];
     username = doc.hostname.split("_")[0];
     time = new Date(Number(doc.time)*1000);
@@ -66,9 +106,20 @@ class CouchDbResultWriter(ResultWriter):
     emit([username, hostname, time], {output: output, severity: severity});
   }
 }"""
+        host_status_map_function = """function(doc){
+  var time, output, hostname, severity, username;
+  if(doc.icingacmd_type == "PROCESS_HOST_CHECK_RESULT" && !doc.object_id){
+    hostname = doc.hostname.split("_")[1];
+    username = doc.hostname.split("_")[0];
+    time = new Date(Number(doc.time)*1000);
+    output = doc.output;
+    severity = doc.severity_code;
+    emit([username, hostname], {output: output, severity: severity, time: time});
+  }
+}"""
         severity_map_function = """function(doc){
   var time, output, hostname, severity, check_name, username;
-  if(doc.icingacmd_type == "PROCESS_SERVICE_CHECK_RESULT"){
+  if(doc.icingacmd_type == "PROCESS_SERVICE_CHECK_RESULT" && doc.object_id){
     hostname = doc.hostname.split("_")[1];
     username = doc.hostname.split("_")[0];
     check_name = doc.check.name;
@@ -78,17 +129,23 @@ class CouchDbResultWriter(ResultWriter):
     emit([username, check_name, time], severity);
   }
 }"""
-	severity_reduce_function = """function(keys, values, rereduce){
+        severity_reduce_function = """function(keys, values, rereduce){
   if (rereduce) {
     return values.reduce(function(a, b){return Math.min(a, b)}, 2);
   } else {
     return Math.min.apply(null, values);
   }
 }"""
+        if "check_result_history" not in design_doc.list_views():
+            design_doc.add_view("check_result_histtory", check_result_history_map_function)
         if "check_results" not in design_doc.list_views():
             design_doc.add_view("check_results", check_results_map_function)
+        if "host_status_history" not in design_doc.list_views():
+            design_doc.add_view("host_status_history", host_status_history_map_function)
         if "host_status" not in design_doc.list_views():
             design_doc.add_view("host_status", host_status_map_function)
         if "severity" not in design_doc.list_views():
-            design_doc.add_view("severity", severity_map_function, reduce_func=severity_reduce_function)
-	design_doc.save()
+            design_doc.add_view("severity",
+                                severity_map_function,
+                                reduce_func=severity_reduce_function)
+        design_doc.save()
